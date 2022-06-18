40dc0a88f1119c446923ee3c08c673aa19174604
AbdelazizBs <bensalemabdelaziz97@gmail.com>
2022-06-18 16:21:55 +0200

    first Push

---
+++ src/main/java/com/iris/irisback/IrisBackApplication.java
@@ -0,0 +1,13 @@
+package com.iris.irisback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrisBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrisBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iris/irisback/IrisBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iris.irisback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrisBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
